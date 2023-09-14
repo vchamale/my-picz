6d7502c4a7b177d22d8f1cdc724cd53410819b13
chamalevictor <chamale.victor@gmail.com>
2023-09-13 21:52:06 -0600

    feat: cloud storage

--- backend/helpers/uploadImage.js
+++ backend/helpers/uploadImage.js
@@ -1,31 +1,31 @@
-import { gc } from '../configs/cloudStorage.js'
-import { v4 } from 'uuid'
+import { gc } from "../configs/cloudStorage.js";
+import { v4 } from "uuid";
 
 const uploadImage = ({ file }, id_end_user) => {
   return new Promise((resolve, reject) => {
-    const bucket = gc.bucket('my-picz')
+    const bucket = gc.bucket("proyecto-aseguramiento-calidad");
 
     if (!file) {
-      reject('Por favor, agrega un archivo.')
-      return
+      reject("Por favor, agrega un archivo.");
+      return;
     }
 
-    const { name, data } = file
+    const { name, data } = file;
 
-    const temp = name.split('.')
+    const temp = name.split(".");
 
-    const extension = temp[temp.length - 1]
+    const extension = temp[temp.length - 1];
 
-    const gcsName = `${id_end_user}/${v4()}` + '.' + extension
-    const blob = bucket.file(gcsName)
-    const blobStream = blob.createWriteStream()
+    const gcsName = `${id_end_user}/${v4()}` + "." + extension;
+    const blob = bucket.file(gcsName);
+    const blobStream = blob.createWriteStream();
 
-    blobStream.on('finish', () => {
-      resolve(blob.publicUrl())
-    })
+    blobStream.on("finish", () => {
+      resolve(blob.publicUrl());
+    });
 
-    blobStream.end(data)
-  })
-}
+    blobStream.end(data);
+  });
+};
 
-export { uploadImage }
+export { uploadImage };
